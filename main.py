50bde62a86128be8209a5574aed1357560a1cbda
bschelske <103594627+bschelske@users.noreply.github.com>
2024-03-25 16:26:31 -0500

    adapt to UI, added comments

--- main.py
+++ main.py
@@ -21,54 +21,58 @@ from tracking import tracking, export_to_csv, get_frames
 from background_subtraction import nd2_background_subtraction
 import ui
 
-# Create UI and handle errors
+# Create UI and handle errors. Information from the UI is stored within the class-instance "app"
 try:
     app = ui.create_UI()
 except ValueError as e:
     print("An error occurred in the UI:", e)
     exit(1)
 
-# Utilize inputs from UI
+# Utilize inputs from UI (might be able to just pass "app" into tracking()... )
 ROI = app.get_roi()
 input_file_path = app.file_path
 input_folder_path = app.folder_path.get()
 canny_lower = app.canny_lower.get()
 canny_upper = app.canny_upper.get()
+save_overlay = app.save_overlay.get()
 
-
-# # Parameters for tracking function
+# Additional parameters for tracking function
+# Spots are rectangular regions to be ignored in tracking. (likely obsolete with inclusion of background subtraction)
 spots = []  # spot in spots = (x,y,w,h)
 output_path = "nd2_results/frame_"  # If overlay = true, save here
 
+# Input handling
 if input_folder_path:
     files = [os.path.join(input_folder_path, f) for f in os.listdir(input_folder_path)]
 if input_file_path:
     files = input_file_path
 
+# Iterate through all input files
 for index, nd2_file in enumerate(files):
-    print(f"Processing file {index + 1} of {len(files)} in brittanys files: {nd2_file}")
-    # Get the filename from brittany's files
+    print(f"Processing file {index + 1} of {len(files)}: {nd2_file}")
+    # Get the filename from file
     file_name = os.path.basename(nd2_file[:-4])
 
     # Make temp folder for images
     temp_dir = tempfile.mkdtemp()
     print("Temporary directory created:", temp_dir)
 
-    # Convert nd2 file to png files and store into a folder
-    nd2_background_subtraction(nd2_file, temp_dir)  # save to temp
+    # Convert nd2 file to png files and store into the temp folder
+    nd2_background_subtraction(nd2_file, temp_dir)
 
-    # Load frames from temp
+    # Load png files from temp folder
     frame_directory = temp_dir
     print(f'Getting frames from {frame_directory}')
     frames = get_frames(parent_dir=frame_directory)
 
     # Perform tracking
-    print("Tracking")
+    print("Tracking...")
     overlay_frames, object_final_position, active_id_trajectory = tracking(frames, output_path, ROI, spots, canny_upper,
                                                                            canny_lower, draw_ROI=False,
-                                                                           save_overlay=False)
-    print("Creating csv files")
+                                                                           save_overlay=save_overlay)
+
     # Create csv file from tracking info
+    print("Creating csv files")
     csv_filename = f"results/{file_name}_results.csv"
     export_to_csv(object_final_position, csv_filename)
     print(f"{csv_filename} saved")
@@ -80,9 +84,6 @@ for index, nd2_file in enumerate(files):
 
     shutil.rmtree(temp_dir)
     print("Temporary directory deleted:", temp_dir)
-
-# ffmpeg to video code
-# ffmpeg -framerate 10 -i frame_%d.png tracking.mp4
 
 # TODO: diagnose overcounting
 # TODO: GPU acceleration :)
