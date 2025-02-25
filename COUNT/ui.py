1479c476d5768c75b317972a833832eb6593ec52
bschelske <103594627+bschelske@users.noreply.github.com>
2025-02-25 11:26:01 -0600

    save/load default settings. removed unused ROI features

--- COUNT/ui.py
+++ COUNT/ui.py
@@ -1,4 +1,5 @@
 import os
+import json
 import tkinter as tk
 from tkinter import filedialog
 
@@ -17,25 +18,44 @@ The UI is used to gather parameters for the tracking.
 
 class ROISelectionApp:
     """Contains all information to begin tracking, collected in the user interface"""
+
     def __init__(self, master):
         """Initiate the class and define default values for tracking parameters"""
         self.master = master
+        self.SETTINGS_PATH = "settings.json"
+        self.load_settings()
         self.file_path = ""
         self.folder_path = tk.StringVar()  # Variable to store the selected folder path
-        self.csv_folder_path = tk.StringVar(value="results/")  # Default csv save path
+        self.csv_folder_path = tk.StringVar(value=self.settings.get("csv_save_path"))  # Default csv save path
         self.overlay_path = ""
-        self.roi_x = tk.IntVar(value=0)  # Default value for ROI X
-        self.roi_y = tk.IntVar(value=0)   # Default value for ROI Y
         self.roi_height = tk.IntVar(value=2048)  # Default value for ROI Height
-        self.roi_width = tk.IntVar(value=2048)    # Default value for ROI Width
-        self.canny_upper = tk.IntVar(value=255)  # Default value for upper canny threshold (3:1 ratio)
-        self.canny_lower = tk.IntVar(value=85)    # Default value for lower canny threshold
-        self.max_centroid_distance = tk.IntVar(value=70)  # max distance an object will travel between frames (px)
-        self.timeout = tk.IntVar(value=5)    # How long before an object is considered lost (frames)
-        self.cell_radius = tk.IntVar(value=6)
-        self.save_overlay = tk.BooleanVar()
+        self.roi_width = tk.IntVar(value=2048)  # Default value for ROI Width
+        self.canny_upper = tk.IntVar(
+            value=self.settings.get("canny_upper"))  # Default value for upper canny threshold (3:1 ratio)
+        self.canny_lower = tk.IntVar(value=self.settings.get("canny_lower"))  # Default value for lower canny threshold
+        self.max_centroid_distance = tk.IntVar(
+            value=self.settings.get("max_centroid_distance"))  # max distance an object will travel between frames (px)
+        self.timeout = tk.IntVar(
+            value=self.settings.get("timeout"))  # How long before an object is considered lost (frames)
+        self.cell_radius = tk.IntVar(value=self.settings.get("cell_radius"))
+        self.save_overlay = tk.BooleanVar(value=self.settings.get("save_overlay"))
         self.files = []  # Empty list that will accept an individual file, or files from a folder
+        self.create_toolbar()
         self.create_widgets()  # This is the part of the UI you can see
+
+    def create_toolbar(self):
+        menu = tk.Menu(self.master)
+        self.master.config(menu=menu)
+
+        file_ = tk.Menu(menu, tearoff=0)
+        menu.add_cascade(label='File', menu=file_)
+        file_.add_command(label='Save as default settings', command=self.save_settings)
+        file_.add_command(label='Load settings', command=self.choose_settings_file)
+        file_.add_command(label='Exit', command=self.master.quit)
+
+        help_ = tk.Menu(menu, tearoff=0)
+        menu.add_cascade(label='Help', menu=help_)
+        help_.add_command(label='Help', command=self.help)
 
     def create_widgets(self):
         """Makes the visual parts of the UI. Ignore unless changing position of UI elements"""
@@ -54,64 +74,40 @@ class ROISelectionApp:
         self.csv_button = tk.Button(self.master, text="Choose Folder", command=self.choose_csv_output)
         self.csv_button.grid(row=0, column=3, padx=5, pady=5)
 
-        # ROI X input field
-        tk.Label(self.master, text="ROI X (px):").grid(row=2, column=0, padx=5, pady=5)
-        self.roi_x_entry = tk.Entry(self.master, textvariable=self.roi_x)
-        self.roi_x_entry.grid(row=2, column=1, padx=5, pady=5)
-
-        # ROI Y input field
-        tk.Label(self.master, text="ROI Y (px):").grid(row=3, column=0, padx=5, pady=5)
-        self.roi_y_entry = tk.Entry(self.master, textvariable=self.roi_y)
-        self.roi_y_entry.grid(row=3, column=1, padx=5, pady=5)
-
-        # ROI Height input field
-        tk.Label(self.master, text="ROI Height (px):").grid(row=4, column=0, padx=5, pady=5)
-        self.roi_height_entry = tk.Entry(self.master, textvariable=self.roi_height)
-        self.roi_height_entry.grid(row=4, column=1, padx=5, pady=5)
-
-        # ROI Width input field
-        tk.Label(self.master, text="ROI Width (px):").grid(row=5, column=0, padx=5, pady=5)
-        self.roi_width_entry = tk.Entry(self.master, textvariable=self.roi_width)
-        self.roi_width_entry.grid(row=5, column=1, padx=5, pady=5)
-
-        # ROI Preview button
-        tk.Label(self.master, text="Define ROI on image").grid(row=6, column=0, padx=5, pady=5)
-        self.roi_button = tk.Button(self.master, text="Preview ROI", command=self.preview_roi)
-        self.roi_button.grid(row=6, column=1, padx=5, pady=5)
-
         # Preview Edge Dectection button
-        tk.Label(self.master, text="Visualize Edge Detection").grid(row=7, column=2, padx=5, pady=5)
+        tk.Label(self.master, text="Visualize Edge Detection").grid(row=7, column=0, padx=5, pady=5)
         self.edge_preview_button = tk.Button(self.master, text="Preview Detection", command=self.preview_edge_detection)
-        self.edge_preview_button.grid(row=7, column=3, padx=5, pady=5)
+        self.edge_preview_button.grid(row=7, column=1, padx=5, pady=5)
 
         # Canny Upper input field
-        tk.Label(self.master, text="Canny Upper:").grid(row=2, column=2, padx=5, pady=5)
+        tk.Label(self.master, text="Canny Upper:").grid(row=2, column=0, padx=5, pady=5)
         self.canny_upper_entry = tk.Entry(self.master, textvariable=self.canny_upper)
-        self.canny_upper_entry.grid(row=2, column=3, padx=5, pady=5)
+        self.canny_upper_entry.grid(row=2, column=1, padx=5, pady=5)
 
         # Canny Lower input field
-        tk.Label(self.master, text="Canny Lower:").grid(row=3, column=2, padx=5, pady=5)
+        tk.Label(self.master, text="Canny Lower:").grid(row=3, column=0, padx=5, pady=5)
         self.canny_lower_entry = tk.Entry(self.master, textvariable=self.canny_lower)
-        self.canny_lower_entry.grid(row=3, column=3, padx=5, pady=5)
+        self.canny_lower_entry.grid(row=3, column=1, padx=5, pady=5)
 
         # Max Centroid Distance input field
-        tk.Label(self.master, text="Max Centroid Distance (px):").grid(row=4, column=2, padx=5, pady=5)
+        tk.Label(self.master, text="Max Centroid Distance (px):").grid(row=4, column=0, padx=5, pady=5)
         self.max_centroid_distance_entry = tk.Entry(self.master, textvariable=self.max_centroid_distance)
-        self.max_centroid_distance_entry.grid(row=4, column=3, padx=5, pady=5)
+        self.max_centroid_distance_entry.grid(row=4, column=1, padx=5, pady=5)
 
         # Timeout threshold input field
-        tk.Label(self.master, text="Timeout Threshold (frames):").grid(row=5, column=2, padx=5, pady=5)
+        tk.Label(self.master, text="Timeout Threshold (frames):").grid(row=5, column=0, padx=5, pady=5)
         self.timeout_entry = tk.Entry(self.master, textvariable=self.timeout)
-        self.timeout_entry.grid(row=5, column=3, padx=5, pady=5)
+        self.timeout_entry.grid(row=5, column=1, padx=5, pady=5)
 
         # Expected Cell Radius input field
-        tk.Label(self.master, text="Expected Cell Radius (px):").grid(row=6, column=2, padx=5, pady=5)
+        tk.Label(self.master, text="Expected Cell Radius (px):").grid(row=6, column=0, padx=5, pady=5)
         self.cell_radius_entry = tk.Entry(self.master, textvariable=self.cell_radius)
-        self.cell_radius_entry.grid(row=6, column=3, padx=5, pady=5)
+        self.cell_radius_entry.grid(row=6, column=1, padx=5, pady=5)
 
         # Save overlay checkbox
-        self.save_overlay_checkbox = tk.Checkbutton(self.master, text="Save Overlay?", variable=self.save_overlay,  onvalue=True, offvalue=False, command=self.on_checkbox_click)
-        self.save_overlay_checkbox.grid(row=8, column=1, columnspan=1, padx=5, pady=5)
+        self.save_overlay_checkbox = tk.Checkbutton(self.master, text="Save Overlay?", variable=self.save_overlay,
+                                                    onvalue=True, offvalue=False, command=self.on_checkbox_click)
+        self.save_overlay_checkbox.grid(row=8, column=0, columnspan=1, padx=5, pady=5)
 
         # Button to confirm selections
         self.confirm_button = tk.Button(self.master, text="      Confirm     ", command=self.confirm_selections)
@@ -121,9 +117,43 @@ class ROISelectionApp:
         self.quit_button = tk.Button(self.master, text="        Quit        ", command=self.quit_ui)
         self.quit_button.grid(row=8, column=3, padx=1, pady=10)
 
-    # Some functions of the UI require functions
+    def choose_settings_file(self):
+        self.SETTINGS_PATH = filedialog.askopenfilename(filetypes=[("JSON files", "*.json")])
+        self.load_settings()
+        tk.messagebox.showinfo("Settings", f"Settings loaded from:\n{self.SETTINGS_PATH}")
+
+    def load_settings(self):
+        try:
+            with open(self.SETTINGS_PATH, "r") as file:
+                self.settings = json.load(file)
+        except FileNotFoundError:
+            print("no settings.json found... using defaults")
+            self.settings = {"canny_upper": 255,
+                             "canny_lower": 85,
+                             "max_centroid_distance": 70,
+                             "timeout": 5,
+                             "cell_radius": 6,
+                             "save_overlay": False,
+                             "csv_save_path": "results/"}
+
+    def save_settings(self):
+        settings = {"canny_upper": self.canny_upper.get(),
+                             "canny_lower": self.canny_lower.get(),
+                             "max_centroid_distance": self.max_centroid_distance.get(),
+                             "timeout": self.timeout.get(),
+                             "cell_radius": self.cell_radius.get(),
+                             "save_overlay": self.save_overlay.get(),
+                            "csv_save_path": self.csv_folder_path.get()
+        }
+        with open(self.SETTINGS_PATH, "w") as file:
+            json.dump(settings, file, indent=4)
+        tk.messagebox.showinfo("Settings", f"Settings saved to\n{self.SETTINGS_PATH}")
+
+    def help(self):
+        tk.messagebox.showinfo("Help", "no help here")
+
     def choose_file(self):
-        self.file_path = filedialog.askopenfilename()
+        self.file_path = filedialog.askopenfilename(filetypes=[("ND2 files", "*.nd2")])
         with ND2Reader_SDK(self.file_path) as nd2_file:
             self.roi_width.set(nd2_file.metadata['width'])
             self.roi_height.set(nd2_file.metadata['height'])
@@ -154,13 +184,13 @@ class ROISelectionApp:
 
         # Ensure the output directory exists
         os.makedirs(self.csv_folder_path.get(), exist_ok=True)
-        os.makedirs(self.csv_folder_path.get()+"overlay/", exist_ok=True)
-        self.overlay_path = self.csv_folder_path.get()+"overlay/"
-        os.makedirs(self.csv_folder_path.get()+"final_results/", exist_ok=True)
+        os.makedirs(self.csv_folder_path.get() + "overlay/", exist_ok=True)
+        self.overlay_path = self.csv_folder_path.get() + "overlay/"
+        os.makedirs(self.csv_folder_path.get() + "final_results/", exist_ok=True)
 
         print(".csv results save path:", self.csv_folder_path.get())
-        print("ROI X:", self.roi_x.get())
-        print("ROI Y:", self.roi_y.get())
+        # print("ROI X:", self.roi_x.get())
+        # print("ROI Y:", self.roi_y.get())
         print("ROI Height:", self.roi_height.get())
         print("ROI Width:", self.roi_width.get())
         print("Canny Upper:", self.canny_upper.get())
@@ -229,7 +259,7 @@ class ROISelectionApp:
         backSub = cv2.createBackgroundSubtractorMOG2(varThreshold=16, detectShadows=False)
         with ND2Reader_SDK(self.files[0]) as nd2_file:
             # MOG2 Background Method:
-            for frame in nd2_file[:frame_index+1]:  # First five frames to calculate background
+            for frame in nd2_file[:frame_index + 1]:  # First five frames to calculate background
                 frame = cv2.normalize(frame, None, 0, 255, cv2.NORM_MINMAX, dtype=cv2.CV_8U)
                 foreground_mask = backSub.apply(frame)
 
@@ -259,7 +289,8 @@ class ROISelectionApp:
             for cnt in contours:
                 cv2.drawContours(frame_copy, [cnt], 0, (0, 0, 255), 2)
 
-            cv2.putText(frame_copy, str(f"Frame {frame_index+1} Objects: {len(contours)}"), (10,40), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2, cv2.LINE_AA)
+            cv2.putText(frame_copy, str(f"Frame {frame_index + 1} Objects: {len(contours)}"), (10, 40),
+                        cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2, cv2.LINE_AA)
         return frame_copy
 
     def quit_ui(self):
